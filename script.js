717e5b880664c4abb95cde444552bb2380ef01e8
Naet27 <https://github.com/Naet27/Cake.git>
2025-09-21 04:24:28 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+// เปิดไมค์เพื่อฟังเสียง
+navigator.mediaDevices.getUserMedia({ audio: true }).then(stream => {
+  const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+  const analyser = audioContext.createAnalyser();
+  const microphone = audioContext.createMediaStreamSource(stream);
+  const dataArray = new Uint8Array(analyser.frequencyBinCount);
+
+  microphone.connect(analyser);
+
+  function detectBlow() {
+    analyser.getByteFrequencyData(dataArray);
+    let volume = dataArray.reduce((a, b) => a + b) / dataArray.length;
+
+    if (volume > 50) { // ถ้าเสียงดังเหมือนเป่า
+      document.querySelector(".flame").style.display = "none";
+      document.getElementById("message").innerText = "Happy Birthday To Milk";
+    } else {
+      requestAnimationFrame(detectBlow);
+    }
+  }
+
+  detectBlow();
+}).catch(err => {
+  alert("ไม่สามารถเปิดไมค์ได้: " + err);
+});
